a9afa3ef078d4dc5eca8643abac94b1f29752d84
Christina Meliniotou <chris.melinioti@gmail.com>
2019-11-26 20:00:40 +0100

    Update db_conn.php

--- src/includes/db_conn.php
+++ src/includes/db_conn.php
@@ -6,6 +6,6 @@ DEFINE ('DB_HOST', '127.0.0.1');
 DEFINE ('DB_NAME', 'marveldatabase');
 
 $con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD , DB_NAME) or 
-die ('Could not connect with database: ' . mysqli_error($con));
+die ('Could not connect with database: ' . mysqli_connect_error());
     mysqli_select_db($con, DB_NAME);
 ?>
