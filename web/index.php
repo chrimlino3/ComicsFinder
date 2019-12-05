e0faabc6c7a39cbcc68cdc4d526d0d147e20ba0b
Christina Meliniotou <47558364+chrimlino3@users.noreply.github.com>
2019-12-04 15:51:32 -1200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -13,6 +13,10 @@ require_once(__DIR__ . '/../CSS/style.php');
 $min_length = 3;
 $input = !empty($_GET['c']) ? $_GET['c'] : '';
 $input = mysqli_real_escape_string($con, $input);
+$title = !empty($_POST['title']) ? $_POST['title'] : '';
+$body = !empty($_POST['body']) ? $_POST['body'] : '';
+$title = mysqli_real_escape_string($con, $title);
+$body = mysqli_real_escape_string($con, $body);
 
 ?>
 <div class="container h-100">
@@ -29,54 +33,53 @@ $input = mysqli_real_escape_string($con, $input);
 </div>
 <?php
 
-
-if(strlen($input) >= $min_length) {
-    $input = htmlspecialchars($input);
-
-    $raw_results = mysqli_query($con, "SELECT `title`, `issue`, `description`, `thumbnail`, `characters`, `thumbnail`, `extension` 
-                                        FROM comics Where (`characters` LIKE '%".$input."%')") or die(mysqli_error($con));
-
-if (mysqli_num_rows($raw_results) > 0){
-    while($results = mysqli_fetch_array($raw_results)) {
-        '<div class="results">' .
-        print "<p>__________________________________________</p>".
-              "<p><h3>" .$results['title']. "</h3></p>" .
-              "<p class='col-md-8'>" .$results['description']. "</p>".
-              '<div class="image"><img height="300" width="300" src="' .  $results['thumbnail'] . '.' . $results['extension'] . '"/></div>' .
-        '</div>';
+   
+    $sql = "INSERT INTO comics (`review_title`,`review_body`) VALUES ('$title','$body')";
+    if (!mysqli_query($con, $sql)) {
+        print "Error: " . mysqli_error($con);
     }
-} else {
-    print "No results";
-}
+
+    if(strlen($input) >= $min_length) {
+        $input = htmlspecialchars($input);
+        $raw_results = mysqli_query($con, "SELECT `title`, `issue`, `description`, `thumbnail`, `characters`, `thumbnail`, `extension` 
+                                            FROM comics Where (`characters` LIKE '%".$input."%')");
+
+    if (mysqli_num_rows($raw_results) > 0){
+        while($results = mysqli_fetch_array($raw_results)) {
+            '<div class="results">' .
+            print "<p><h3>" .$results['title']. "</h3></p>".
+                  "<p class='col-md-8'>" .$results['description']. "</p>".
+                  '<div class="image"><img height="300" width="300" src="' .  $results['thumbnail'] . '.' . $results['extension'] . '"/></div>'.
+            '</div>';
+
+            '<div class="postreview ml-8">' .
+                print '<form method="POST">' .
+                    '<input class="form-control" type="text" name="title" placeholder="Title"/>' . "\n<br />" .
+                    '<input class="form-control" type="text" name="body" placeholder="Comment"/>' . "\n<br />" .
+                    '<input class="button" type="submit" name="submit" value="Write a review"/>' .
+                    '<input type = "reset" value="Cancel">' .
+                    '</form>' .
+            '</div>';
+            
+                if(isset($_POST['submit'])) {
+                    $sql1 = mysqli_query($con, "SELECT `review_title`, `review_body` FROM comics");
+                    echo "<meta http=equiv='refresh' content='0'";
+                    while($reviews = mysqli_fetch_array($sql1)) {
+                        print "<p><h3>" .$reviews['review_title']. "</h3></p>".
+                              "<p class='col-md-8'>" .$reviews['review_body']. "</p>";
+                    }
+                }
+        }
+
+    } else {
+        print "No results";
+    }
+
 } else {
     if (!empty($input)) {
         print "Minimum length is " . $min_length;
     }
 }
-
-$title = !empty($_GET['title']) ? $_GET['title'] : '';
-$body = !empty($_GET['body']) ? $_GET['body'] : ''; 
-$title = mysqli_real_escape_string($con, $title);
-$body = mysqli_real_escape_string($con, $body);
-
-$sql = mysqli_query($con, "INSERT INTO reviews (`title`,`body`) VALUES ('$title','$body')") or die(mysqli_error($con));
-
-    '<div class="postreview ml-8">' .
-        print '<form method="GET">' .
-              '<input class="form-control" type="text" name="title" placeholder="Best comic ever"/>' . "\n<br />" .
-              '<input class="form-control" type="text" name="body" placeholder="Best comic ever"/>' . "\n<br />" .
-              '<input class="button" type="submit" name="submit" value="Write a review"/>' .
-              '</form>' .
-    '</div>';
-
-$sql1 = mysqli_query($con, "SELECT `title`, `body` FROM reviews") or die(mysqli_error($con));
-while($reviews = mysqli_fetch_array($sql1)) {
-    print "<p>__________________________________________</p>";
-    print "<p><h3>" .$reviews['title']. "</h3></p>";
-    print "<p class='col-md-8'>" .$reviews['body']. "</p>";
-
-}
-
     
 ?>
 </body>
