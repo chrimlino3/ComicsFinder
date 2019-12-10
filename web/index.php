24303aa61f98fb0a3ac65d17320d01df0ada5691
Christina Meliniotou <47558364+chrimlino3@users.noreply.github.com>
2019-12-09 14:39:40 -1200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -7,16 +7,25 @@ require_once(__DIR__ . '/../CSS/style.php');
 $min_length = 3;
 $input = !empty($_GET['c']) ? $_GET['c'] : '';
 $input = mysqli_real_escape_string($con, $input);
-$title = !empty($_POST['title']) ? $_POST['title'] : '';
-$title = mysqli_real_escape_string($con, $title);
-$body = !empty($_POST['body']) ? $_POST['body'] : '';
-$body = mysqli_real_escape_string($con, $body);
+
+if(isset($_POST['submit'])) {
+    print_r($_POST);
+    $title = !empty($_POST['title']) ? $_POST['title'] : '';
+    $title = mysqli_real_escape_string($con, $title);
+    $body = !empty($_POST['body']) ? $_POST['body'] : '';
+    $body = mysqli_real_escape_string($con, $body);
+    
+    $marvelid = !empty($_POST['marvelid']) ? $_POST['marvelid'] : ''; // marvelid comes from comics table. Therefore we need to retrieve it from there and insert it in the reviews column marvelid.
+    $insert = mysqli_query($con, "INSERT INTO reviews (`title`, `body`, `marvelid`) VALUES ('$title', '$body', '$marvelid')"); 
+    print "Added: " . "title: " . $title . "body: " . $body . "marvelid: " . $marvelid . "\n";
+    exit;
+}
 
 ?>
 <div class="container h-100">
     <form method="GET">
     <div class="d-flex justify-content-center h-100">
-        <h2>Broswe your favorite superhero stories</h2>
+        <h2>Browse your favorite superhero stories</h2>
         <h5>Hulk, X-Men, Wolverine, Wasp (Ultimate), Spider-Man</h5>
         <div class="searchbar">
             <input class="search_input" type="text" name="c" placeholder="3-D Man" />
@@ -27,9 +36,9 @@ $body = mysqli_real_escape_string($con, $body);
 </div>
 <?php
 
-    if(strlen($input) >= $min_length) {
-        $input = htmlspecialchars($input);
-        $raw_results = mysqli_query($con, "SELECT `title`, `issue`, `description`, `thumbnail`, `characters`, `thumbnail`, `extension`, `marvelid` 
+if(strlen($input) >= $min_length) {
+    $input = htmlspecialchars($input);
+    $raw_results = mysqli_query($con, "SELECT `title`, `issue`, `description`, `thumbnail`, `characters`, `thumbnail`, `extension`, `marvelid` 
                                             FROM comics Where (`characters` LIKE '%".$input."%')");
 
     if (mysqli_num_rows($raw_results) > 0){
@@ -41,31 +50,17 @@ $body = mysqli_real_escape_string($con, $body);
                   '<div class="image"><img height="300" width="300" src="' .  $results['thumbnail'] . '.' . $results['extension'] . '"/></div>'.
             '</div>';
 
-            $marvelid = $results['marvelid'];
-
             '<div class="postreview ml-8">' .
             print '<form method="POST">' .
-            '<input class="form-control" type="text" name="title" placeholder="Title"/>' . "\n<br />" .
-            '<input class="form-control" type="text" name="body" placeholder="Comment"/>' . "\n<br />" .
-            '<input class="button" type="submit" name="submit" value="Write a review"/>' .
-            '<input type="hidden" name="marvelid" value=' . $marvelid . '/>' .
-            '<input type = "reset" value="Cancel">' .
-            '</form>' .
+                    '<input class="form-control" type="text" name="title" placeholder="Title"/>' . "\n<br />" .
+                    '<input class="form-control" type="text" name="body" placeholder="Comment"/>' . "\n<br />" .
+                    '<input class="button" type="submit" name="submit" value="Write a review"/>' .
+                    '<input type="hidden" name="marvelid" value="' . $results['marvelid'] . '"/>' .
+                    '<input type="reset" value="Cancel">' .
+                    '</form>' .
             '</div>';
-
-            if(isset($_POST['submit'])) {
-                $insert = mysqli_query($con, "INSERT INTO reviews (`title`, `body`, `marvelid`) VALUES ('$title', '$body', '$marvelid')"); // insert comment in the reviews table. Duplicate entry upon refresh.
-                print "Added: " . "title: " . $title . "body: " . $body . "marvelid: " . $marvelid . "\n";
-            }
-            
-            $sql1 = mysqli_query($con, "SELECT title, body, marvelid FROM reviews"); // Select title and body from reviews 
-            echo "<meta http=equiv='refresh' content='0'";
-            while($reviews = mysqli_fetch_array($sql1)) {
-                print "<p><h3>" .$reviews['title']. "</h3></p>". // show it in UI. 
-                    "<p class='col-md-8'>" .$reviews['body']. "</p>";
-    }
         }
-
+    
     } else {
         print "No results";
     }
@@ -75,4 +70,10 @@ $body = mysqli_real_escape_string($con, $body);
         print "Minimum length is " . $min_length;
     }
 }
+$sql1 = mysqli_query($con, "SELECT title, body, marvelid FROM reviews"); // Select title and body from reviews 
+echo "<meta http=equiv='refresh' content='0'";
+while($reviews = mysqli_fetch_array($sql1)) {
+    print "<p><h3>" .$reviews['title']. "</h3></p>". // show it in UI. 
+        "<p class='col-md-8'>" .$reviews['body']. "</p>";
 
+}
