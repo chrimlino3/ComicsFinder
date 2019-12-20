9557df20215538e2e40bea638e2f6923a60de23d
ChrisMeliniotou <chris.melinioti@gmail.com>
2019-12-20 11:36:38 +0100

    refactoring code

--- src/includes/star_interface.php
+++ src/includes/star_interface.php
@@ -1,48 +1,6 @@
 <?php
 require_once(__DIR__ . '/../../vendor/autoload.php');
 require_once(__DIR__ . '/../../web/index.php');
-
-?> 
-    <div class="row">
-    <div class="col-sm-12">
-    <form id="ratingForm" method="POST">
-    <div class="form-group">
-    <h4>Comments</h4>
-    <button name="stars" type="button" class="btn btn-warning btn-sm rateButton" aria-label="Left Align">
-    <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-    </button>
-    <button name="stars" type="button" class="btn btn-default btn-grey btn-sm rateButton" aria-label="Left Align">
-    <span class="glyphicon glyphicon-star" aria-hidden="true"> </span>
-    </button>
-    <button name="stars" type="button" class="btn btn-default btn-grey btn-sm rateButton" aria-label="Left Align">
-    <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-    </button>
-    <button name="stars" type="button" class="btn btn-default btn-grey btn-sm rateButton" aria-label="Left Align">
-    <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-    </button>
-    <button name="stars" type="button" class="btn btn-default btn-grey btn-sm rateButton" aria-label="Left Align">
-    <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-    </button>
-
-    <input name="stars" type="hidden" class="form-control" id="stars" name="stars" value=<?$stars?>>
-    <input type="hidden" name="marvelid" class="form-control" name="marvelid" value= <?$results['marvelid']?>>
-    </div>
-    <div class="form-group">
-    <label for="usr"></label>
-    <input type="text" class="form-control" id="title" name="title" placeholder="Title" required>
-    </div>
-    <div class="form-group">
-    <label for="body"></label>
-    <textarea class="form-control" rows="5" id="body" name="body" placeholder="Comment" required></textarea>
-    </div>
-    <div class="form-group">
-    <button type="submit" class="btn btn-info" id="saveReview">Save Review</button> <button type="button" class="btn btn-info" id="cancelReview">Cancel</button>
-    </div>
-    </form>
-    </div>
-    </div> 
-
-<?php
 require_once(__DIR__ . '/star_interface.php');
 ?>
 <script type="text/javascript" src="/../../vendor/autoload.php">
